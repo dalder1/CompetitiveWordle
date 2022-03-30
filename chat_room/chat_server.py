57fdca9a41f04fd11a790304fb038e93455967ee
dalder1 <51690677+dalder1@users.noreply.github.com>
2022-03-30 14:41:45 -0400

    small cleanup

--- chat_room/chat_server.py
+++ chat_room/chat_server.py
@@ -1,25 +1,25 @@
 import socket, threading
 
-def accept_client(stop_threads):
+def accept_client():
     while True:
         #accept    
         cli_sock, cli_add = ser_sock.accept()
         CONNECTION_LIST.append(cli_sock)
-        thread_client = threading.Thread(target = broadcast_usr, args=[cli_sock, stop_threads])
+        thread_client = threading.Thread(target = broadcast_usr, args=[cli_sock])
         thread_client.start()
 
-def broadcast_usr(cli_sock, stop_threads):
+def broadcast_usr(cli_sock):
     while True:
         try:
             data = cli_sock.recv(1024)
             if data:
-               b_usr(cli_sock, data)
+               send_to_all_usrs(cli_sock, data)
         except Exception as x:
             print(x.message)
             break
 
 
-def b_usr(cs_sock, msg):
+def send_to_all_usrs(cs_sock, msg):
     for client in CONNECTION_LIST:
         if client != cs_sock:
             client.send(msg)
@@ -39,5 +39,5 @@ if __name__ == "__main__":
     ser_sock.listen(1)
     print('Chat server started on port : ' + str(PORT))
     stop_threads = False
-    thread_ac = threading.Thread(target = accept_client, args=(stop_threads,))
+    thread_ac = threading.Thread(target = accept_client)
     thread_ac.start()
