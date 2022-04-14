0d3960ca48d0bbbb55e1667cd9339271fe8f9758
dalder1 <51690677+dalder1@users.noreply.github.com>
2022-04-14 17:55:21 -0400

    actually merge

--- wordle_ServerClient/wordle_server.py
+++ wordle_ServerClient/wordle_server.py
@@ -5,44 +5,52 @@ from unicodedata import decimal
 
 WORDLIST_FILE = 'wordlist.txt'
 
-def accept_player(word):
-    #accept    
-    cli_sock, cli_add = ser_sock.accept()
-    CONNECTION_LIST.append(cli_sock)
-    thread_client = threading.Thread(target = broadcast_player, args=[cli_sock, word])
+def accept_player(word, server_sock, conn_list):
+    # accept
+    conn, addr = server_sock.accept()
+    # TODO: not thread safe
+    conn_list.append(conn)
+    thread_client = threading.Thread(target = player_thread, args=[conn, word])
     thread_client.start()
+    thread_client.join()
 
-def broadcast_player(cli_sock, word):
+def player_thread(player_sock, word):
     keepPlaying = True
     while keepPlaying:
         try:
-            data = pickle.loads(cli_sock.recv(1024))
+            data = pickle.loads(player_sock.recv(1024))
             if data:
                 guess = data["guess"]
                 numGuesses = data["numGuesses"]
                 response = dict()
                 if (guess == word):
-                    response["response"] = "Great job! You guessed the word in " + numGuesses + " guesses." 
+                    response["response"] = "Great job! You guessed the word in " + str(numGuesses) + " guesses."
+                    # TODO: create new word, continue game
+                    keepPlaying = False
                 elif (numGuesses >= 5):
                     response["response"] = "sorry you have lost, the word was " + word + "."
                     keepPlaying = False
                 else:
                     response["response"] = "try again"
-                send_to_player(cli_sock, pickle.dumps(response))
-                # send_to_all_players(cli_sock, response.encode())
+                send_to_player(player_sock, pickle.dumps(response))
+                # send_to_all_players(player_sock, response.encode())
         except Exception as x:
             print(x.message)
             break
 
-#update of your own score
-def send_to_player(cs_sock, msg):
-    cs_sock.send(msg)
+# send_to_player
+# takes in current player's socket and message, sends message to current player
+def send_to_player(player_sock, msg):
+    player_sock.send(msg)
 
-#update to other players of your score
-def send_to_all_players(cs_sock, msg):
-    for client in CONNECTION_LIST:
+
+# send_to_all_players
+# takes in current player's socket and message, sends message to all players
+# except current
+def send_to_all_players(player_sock, msg):
+    for client in conn_list:
         print("sending")
-        if client != cs_sock:
+        if client != player_sock:
             client.send(msg)
 
 
@@ -54,20 +62,32 @@ def main():
     with open(WORDLIST_FILE) as wordlistFile:
         wordlist = wordlistFile.read().splitlines() 
     word = wordlist[random.randint(0, (len(wordlist) - 1))]
+    print('Chosen word: ' + word)
 
     # --- server socket setup ---
     conn_list = []
 
     # socket
-    ser_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     # bind
     HOST = 'localhost'
     PORT = 5023
-    ser_sock.bind((HOST, PORT))
+    server_sock.bind((HOST, PORT))
 
     # listen    
-    ser_sock.listen(1)
+    server_sock.listen(1)
     print('Wordle server started on port : ' + str(PORT))
-    thread_ac = threading.Thread(target = accept_player, args=(word,))
+    thread_ac = threading.Thread(target = accept_player, args=(word, server_sock, conn_list))
     thread_ac.start()
+    # TODO: accept more connections for more players
+    thread_ac.join()
+
+    for con in conn_list:
+        con.close()
+
+if __name__ == "__main__":
+    main()
+
+    exit(0) 
