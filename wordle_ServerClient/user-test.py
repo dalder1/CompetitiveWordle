684cd2b3defb636e9c7a9840a31d007e337dd0c4
dalder1 <51690677+dalder1@users.noreply.github.com>
2022-04-14 17:55:53 -0400

    User class (#1)
    * basic wordle game from wordle_server adapted to user class
    
    * user class working for one word one board
    
    * works for multiple words
    
    * actually merge

---
+++ wordle_ServerClient/user-test.py
@@ -0,0 +1,18 @@
+import random
+from user import User
+
+WORDLIST_FILE = 'wordlist.txt'
+
+with open(WORDLIST_FILE) as wordlistFile:
+    wordlist = wordlistFile.read().splitlines() 
+# word = wordlist[random.randint(0, (len(wordlist) - 1))]
+words = ["fairy", "judge", "large"]
+game = User("daniel", words)
+for i in range(len(words) * 6):
+    guess = input()
+    response = game.makeGuess(guess)
+    if ("game over" in response[0]):
+        print(response)
+        break
+    print(response)
+
